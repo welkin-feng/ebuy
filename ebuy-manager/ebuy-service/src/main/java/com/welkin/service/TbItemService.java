f1528efcb96f9bfd945a448d454889a9a44b6dc0
Welkin <382523558@qq.com>
2017-07-13 13:10:14 +0800

    Initial commit & update pom.xml

---
+++ ebuy-manager/ebuy-service/src/main/java/com/welkin/service/TbItemService.java
@@ -0,0 +1,25 @@
+package com.welkin.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.welkin.mapper.TbItemMapper;
+import com.welkin.pojo.TbItem;
+import com.welkin.pojo.TbItemExample;
+import com.welkin.pojo.TbItemExample.Criteria;
+
+public class TbItemService {
+	 @Autowired
+	  private TbItemMapper tbItemdao;
+	  
+	//查询所有商品信息
+	  public List<TbItem> findall(){
+		  //mapping生成Example对象查询条件对象的模板
+		  TbItemExample ex=new TbItemExample();
+		  Criteria c = ex.createCriteria();//生成查询条件
+		  c.andTitleLike("%电%");
+		List<TbItem> li = tbItemdao.selectByExample(ex);
+		return li;
+	  }
+}

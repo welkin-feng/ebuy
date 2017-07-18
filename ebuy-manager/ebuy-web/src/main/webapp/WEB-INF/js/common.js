ed5bf3432180d25a867e3687ebe9ce5ef7b16f1e
Welkin <382523558@qq.com>
2017-07-18 15:36:41 +0800

    Update

--- ebuy-manager/ebuy-web/src/main/webapp/WEB-INF/js/common.js
+++ ebuy-manager/ebuy-web/src/main/webapp/WEB-INF/js/common.js
@@ -104,7 +104,7 @@ var TT = TAOTAO = {
     
     // 初始化选择类目组件
     initItemCat : function(data){
-    	$("#selectItemCat").each(function(i,e){
+    	$(".selectItemCat").each(function(i,e){
     		var _ele = $(e);
     		if(data && data.cid){
     			_ele.after("<span style='margin-left:10px;'>"+data.cid+"</span>");

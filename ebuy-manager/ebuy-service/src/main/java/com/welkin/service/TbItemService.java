29c03d9245fa430b448514dd4ad78f5707855f6e
Welkin <382523558@qq.com>
2017-07-20 14:04:22 +0800

    添加 保存“新增商品” 的方法

--- ebuy-manager/ebuy-service/src/main/java/com/welkin/service/TbItemService.java
+++ ebuy-manager/ebuy-service/src/main/java/com/welkin/service/TbItemService.java
@@ -1,9 +1,14 @@
 package com.welkin.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.welkin.mapper.TbItemCatMapper;
@@ -19,13 +24,14 @@ import com.welkin.pojo.TbItemExample.Criteria;
 public class TbItemService {
 	@Autowired
 	private TbItemMapper tbItemdao;
+	@Autowired
 	private TbItemCatMapper catMapper;
 	
 	//查询所有商品信息
 	public List<TbItem> findall(){
 		//mapping生成Example对象查询条件对象的模板
 		TbItemExample ex=new TbItemExample();
-		Criteria c = ex.createCriteria();//生成查询条件
+		Criteria c = ex.createCriteria();	//生成查询条件
 		c.andTitleLike("%电%");
 		List<TbItem> li = tbItemdao.selectByExample(ex);
 		return li;
@@ -41,6 +47,7 @@ public class TbItemService {
 		TPager pager = new TPager();
 		TbItemExample ex = new TbItemExample();
 //		Criteria cri = ex.createCriteria();
+		
 		// 查询前开始分页
 		// page: 第几页, rows：每页的记录数量
 		PageHelper.startPage(page, rows);
@@ -61,7 +68,30 @@ public class TbItemService {
 		// 按父类id查询下级所有分类对象
 		c.andParentIdEqualTo(parent_id);
 		return catMapper.selectByExample(ex);
-		
-		
+	}
+	
+	public String save(TbItem po) {
+		int x = tbItemdao.insert(po);
+		Map<String, Integer> m = new HashMap<String, Integer>();
+		if(x > 0) {
+			m.put("status", 200);
+		} else {
+			m.put("status", 500);
+		}
+		/*
+		 * jackson 组件将java 对象和 json 对象相互转化
+		 * spring 中加入 jackson 组件，并且控制器返回直接 @ResponseBody 自动完成
+		 * 将java 对象转换成 json字符串的工作，但是在不同版本浏览器中存在兼容问题
+		 * 为了解决这个通过jackson 组件手工将java对象转换成json字符串
+		 * ObjectMapper 提供将java对象转换成json字符串的方法 writeValueAsString
+		 * */
+		ObjectMapper om = new ObjectMapper();
+		String s = null;
+		try {
+			s = om.writeValueAsString(m);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return s;
 	}
 }
--- ebuy-manager/ebuy-web/src/main/java/com/welkin/controller/TbItemController.java
+++ ebuy-manager/ebuy-web/src/main/java/com/welkin/controller/TbItemController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.welkin.pojo.TPager;
+import com.welkin.pojo.TbItem;
 import com.welkin.pojo.TbItemCat;
 import com.welkin.pojo.TreeNode;
 import com.welkin.service.TbItemService;
@@ -17,37 +18,45 @@ import com.welkin.service.TbItemService;
 @RequestMapping("/item")
 public class TbItemController {
 	@Autowired
-	private TbItemService tbItemser;
+	private TbItemService tbItemService;
+
+	@RequestMapping("/save")
+	@ResponseBody
+	public String save(TbItem po) {
+		return tbItemService.save(po);
+
+	}
 
 	@RequestMapping("/list")
 	// spring 提供了 @ResponseBody 注解将java的对象自动转换成json对象并发送给客户端
 	@ResponseBody
 	public TPager findlist(int page, int rows) {
 		System.out.println("page:" + page + ",rows:" + rows);
-		TPager p = tbItemser.selectPager(page, rows);
+		TPager p = tbItemService.selectPager(page, rows);
 		return p;
 	}
-	
+
 	// 针对商品分类查询的 mapping
 	@RequestMapping("/cat/list")
 	@ResponseBody
-	public List<TreeNode> findCaps(@RequestParam(value = "cid", defaultValue = "0") Long parent_id) {
-		List<TbItemCat> li = tbItemser.findByParentId(parent_id);
-		
-		List<TreeNode> mli=new ArrayList<TreeNode>();
-		//遍历分类集合
-		for (TbItemCat cat :li) {
-			//用于页面显示数据的封装  id,text,status
-			TreeNode m=new TreeNode();
+	public List<TreeNode> findCaps(@RequestParam(value = "id", defaultValue = "0") Long parent_id) {
+		System.out.println("parent id: " + parent_id);
+		List<TbItemCat> li = tbItemService.findByParentId(parent_id);
+
+		List<TreeNode> mli = new ArrayList<TreeNode>();
+		// 遍历分类集合
+		for (TbItemCat cat : li) {
+			// 用于页面显示数据的封装 id,text,status
+			TreeNode m = new TreeNode();
 			m.setId(cat.getId());
 			m.setText(cat.getName());
-			m.setState(cat.getIsParent()?"closed":"open");
-			//将分类节点数据封装到集合中
+			m.setState(cat.getIsParent() ? "closed" : "open");
+			// 将分类节点数据封装到集合中
 			mli.add(m);
 		}
 		System.out.println("商品分类" + li.size());
-		
+
 		return mli;
 	}
-	
+
 }

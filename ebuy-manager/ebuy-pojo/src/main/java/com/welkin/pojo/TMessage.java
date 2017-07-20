cc872c53ce4f77cd1236bc0afb2913be00fc06b8
Welkin <382523558@qq.com>
2017-07-20 14:01:09 +0800

    服务器返回给前端的状态码对象

---
+++ ebuy-manager/ebuy-pojo/src/main/java/com/welkin/pojo/TMessage.java
@@ -0,0 +1,19 @@
+package com.welkin.pojo;
+
+public class TMessage {
+	private Integer status;
+	private Object data;
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+}

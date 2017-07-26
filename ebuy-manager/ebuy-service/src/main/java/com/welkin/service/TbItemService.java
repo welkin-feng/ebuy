e2eb8c8fcafc5d946a36e1c2faa4d92fc278166e
Welkin <382523558@qq.com>
2017-07-26 09:14:50 +0800

    添加查询商品“描述信息”和查询商品“规格参数详情”方法

--- ebuy-manager/ebuy-service/src/main/java/com/welkin/service/TbItemService.java
+++ ebuy-manager/ebuy-service/src/main/java/com/welkin/service/TbItemService.java
@@ -6,25 +6,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.welkin.mapper.TbItemCatMapper;
 import com.welkin.mapper.TbItemDescMapper;
 import com.welkin.mapper.TbItemMapper;
 import com.welkin.mapper.TbItemParamItemMapper;
-import com.welkin.mapper.TbItemParamMapper;
-import com.welkin.pojo.TMessage;
-import com.welkin.pojo.TPager;
+import com.welkin.pojo.Pager;
 import com.welkin.pojo.TbItem;
 import com.welkin.pojo.TbItemCat;
 import com.welkin.pojo.TbItemCatExample;
 import com.welkin.pojo.TbItemCatExample.Criteria;
 import com.welkin.pojo.TbItemDesc;
 import com.welkin.pojo.TbItemExample;
-import com.welkin.pojo.TbItemParam;
 import com.welkin.pojo.TbItemParamItem;
+import com.welkin.pojo.TbItemParamItemExample;
 
 @Service
 public class TbItemService {
@@ -38,85 +33,91 @@ public class TbItemService {
 	private TbItemParamItemMapper tbItemParamItemMapper;
 	
 	/**
+	 * 根据itemid查询ItemDesc
+	 * @param itemId
+	 * @return
+	 */
+	public TbItemDesc findTbItemDescById(Long itemId) {
+		TbItemDesc itemdesc = tbItemDescMapper.selectByPrimaryKey(itemId);
+		
+		return itemdesc;
+	}
+	
+	/**
+	 * 根据 商品 item_id 查询所对应的 规格参数信息
+	 * @param paramid
+	 * @return
+	 */
+	public TbItemParamItem findItemParamItemById(Long paramid) {
+		TbItemParamItemExample ex = new TbItemParamItemExample();
+		com.welkin.pojo.TbItemParamItemExample.Criteria c = ex.createCriteria();
+		c.andItemIdEqualTo(paramid);
+		List<TbItemParamItem> li = tbItemParamItemMapper.selectByExampleWithBLOBs(ex);
+		
+		if (li == null || li.size() < 1)
+			return null;
+		return li.get(0);
+	}
+
+	/**
 	 * 插入商品信息
 	 * @param po
 	 * @param desc
 	 * @param paramStr 当前商品规格信息
 	 * @return
 	 */
-	public String save(TbItem po, String desc, String paramStr) {
+	public int save(TbItem po, String desc, String paramStr) {
 		po.setId(System.currentTimeMillis());
 		po.setStatus((byte) 1);
 		po.setCreated(new Date());
 		po.setUpdated(new Date());
-		int x = tbItemMapper.insert(po);
-		
-		if(desc != null) {
+		int x1 = tbItemMapper.insert(po);
+		int x2 = 1, x3 = 1;
+
+		if (desc != null) {
 			TbItemDesc tbItemDesc = new TbItemDesc();
 			tbItemDesc.setItemId(po.getId());
 			tbItemDesc.setItemDesc(desc);
 			tbItemDesc.setCreated(new Date());
 			tbItemDesc.setUpdated(new Date());
-			tbItemDescMapper.insert(tbItemDesc);
+			x2 = tbItemDescMapper.insert(tbItemDesc);
 		}
-		if(paramStr != null) {
+		if (paramStr != null) {
 			TbItemParamItem tbItemParamItem = new TbItemParamItem();
 			tbItemParamItem.setItemId(po.getId());
 			tbItemParamItem.setParamData(paramStr);
 			tbItemParamItem.setCreated(new Date());
 			tbItemParamItem.setUpdated(new Date());
-			tbItemParamItemMapper.insert(tbItemParamItem);
+			x3 = tbItemParamItemMapper.insert(tbItemParamItem);
 		}
-		
-		
-		TMessage m = new TMessage();
-		if(x > 0) {
-			m.setStatus(200);
-		} else {
-			m.setStatus(500);
-		}
-		
-		/*
-		 * jackson 组件将java 对象和 json 对象相互转化
-		 * spring 中加入 jackson 组件，并且控制器返回直接 @ResponseBody 自动完成
-		 * 将java 对象转换成 json字符串的工作，但是在不同版本浏览器中存在兼容问题
-		 * 为了解决这个通过jackson 组件手工将java对象转换成json字符串
-		 * ObjectMapper 提供将java对象转换成json字符串的方法 writeValueAsString
-		 * */
-		ObjectMapper om = new ObjectMapper();
-		String s = null;
-		try {
-			s = om.writeValueAsString(m);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-		return s;
+
+		return x1 & x2 & x3;
 	}
-	
+
 	/**
 	 * 功能分页查询商品
-	 * @param page	当前页码
-	 * @param rows	每页显示记录数量
-	 * @return	封装了分页信息的对象
+	 * @param page 当前页码
+	 * @param rows 每页显示记录数量
+	 * @return 封装了分页信息的对象
 	 */
-	public TPager selectPager(Integer page, Integer rows) {
-		TPager pager = new TPager();
-		TbItemExample ex = new TbItemExample();
-//		Criteria cri = ex.createCriteria();
-		
+	public Pager selectPager(Integer page, Integer rows) {
+		Pager pager = new Pager();
 		// 查询前开始分页
 		// page: 第几页, rows：每页的记录数量
 		PageHelper.startPage(page, rows);
+		
+		TbItemExample ex = new TbItemExample();
 		List<TbItem> li = tbItemMapper.selectByExample(ex);
 		// 获取查询集合
 		// PageInfo<要查询的实体类> pi = new PageInfo<>(查询后的集合对象);
-		PageInfo<TbItem> pi = new PageInfo<>(li);
+//		PageInfo<TbItem> pi = new PageInfo<>(li);
 		
 		pager.setRows(li);
-		pager.setTotal(pi.getTotal());
+//		pager.setTotal(pi.getTotal());
+		pager.setTotal((long) tbItemMapper.countByExample(ex));
 		return pager;
 	}
-	
+
 	/**
 	 * 功能：按父id查询当前类型的所有分类
 	 * @param parent_id 父id
@@ -130,5 +131,5 @@ public class TbItemService {
 		c.andParentIdEqualTo(parent_id);
 		return tbItemCatMapper.selectByExample(ex);
 	}
-	
+
 }

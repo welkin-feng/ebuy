be9f23f738949d8d0ec4c3d9e2b06014eadf27eb
Welkin <382523558@qq.com>
2017-07-26 09:13:02 +0800

    Update item param methods
    添加 /item/param/list 对应的Controller和Service方法，用于查询规格信息列表。
    在TbItemParamService 中添加了一个类，用于构建查询规格参数的返回对象（因为查询规格参数需要同时查询数据库中的两张表）。

--- ebuy-manager/ebuy-service/src/main/java/com/welkin/service/TbItemParamService.java
+++ ebuy-manager/ebuy-service/src/main/java/com/welkin/service/TbItemParamService.java
@@ -1,11 +1,19 @@
 package com.welkin.service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.welkin.mapper.TbItemCatMapper;
 import com.welkin.mapper.TbItemParamMapper;
+import com.welkin.pojo.Pager;
+import com.welkin.pojo.TbItemCat;
+import com.welkin.pojo.TbItemCatExample;
 import com.welkin.pojo.TbItemParam;
 import com.welkin.pojo.TbItemParamExample;
 import com.welkin.pojo.TbItemParamExample.Criteria;
@@ -14,7 +22,21 @@ import com.welkin.pojo.TbItemParamExample.Criteria;
 public class TbItemParamService {
 	@Autowired
 	private TbItemParamMapper tbItemParamMapper;
-	
+	@Autowired
+	private TbItemCatMapper tbItemCatMapper;
+
+	/**
+	 * 删除“商品类别”对应的“规格参数”
+	 * @param ids 要删除的规格参数item_param id List
+	 * @return 删除状态
+	 */
+	public int delete(List<Long> ids) {
+		TbItemParamExample ex = new TbItemParamExample();
+		Criteria c = ex.createCriteria();
+		c.andIdIn(ids);
+		return tbItemParamMapper.deleteByExample(ex);
+	}
+
 	/**
 	 * 功能：按商品类型的编号查询商品类型规格表，返回具体类型对应商品规格对象
 	 * @param id 商品类型id
@@ -30,17 +52,107 @@ public class TbItemParamService {
 			return null;
 		return li.get(0);
 	}
-	
+
 	/**
 	 * 功能：保存商品类型规格基本信息
-	 * @param po 商品类型对应基本规格对象
-	 * @return 
+	 * 
+	 * @param po
+	 *            商品类型对应基本规格对象
+	 * @return
 	 */
-	public boolean save(TbItemParam po) {
-		int x = tbItemParamMapper.insert(po);
-		if(x > 0) 
-			return true;
-		else
-			return false;
+	public int save(TbItemParam po) {
+		po.setCreated(new Date());
+		po.setUpdated(new Date());
+		return tbItemParamMapper.insert(po);
 	}
+
+	/**
+	 * 功能分页查询商品
+	 * 
+	 * @param page
+	 *            当前页码
+	 * @param rows
+	 *            每页显示记录数量
+	 * @return 封装了分页信息的对象
+	 */
+	public Pager selectPager(Integer page, Integer rows) {
+		Pager pager = new Pager();
+		// 查询前开始分页
+		// page: 第几页, rows：每页的记录数量
+		PageHelper.startPage(page, rows);
+
+		TbItemParamExample ex = new TbItemParamExample();
+		List<TbItemParam> paramList = tbItemParamMapper.selectByExampleWithBLOBs(ex);
+		
+		List<Long> catIds = new ArrayList<>();
+		for (int i = 0; i < paramList.size(); i ++) {
+			catIds.add(paramList.get(i).getItemCatId());
+		}
+		
+		TbItemCatExample catEx = new TbItemCatExample();
+		com.welkin.pojo.TbItemCatExample.Criteria c = catEx.createCriteria();
+		c.andIdIn(catIds);
+		List<TbItemCat> catList = tbItemCatMapper.selectByExample(catEx);
+		
+		List<TbItemParamDetail> tli = new ArrayList<>();
+		for (int i = 0; i < catList.size(); i ++) {
+			TbItemParamDetail e = new TbItemParamDetail();
+			e.setTbItemParam(paramList.get(i));
+			e.setItemCatName(catList.get(i).getName());
+			tli.add(e);
+		}
+
+		// 获取查询集合
+		// PageInfo<要查询的实体类> pi = new PageInfo<>(查询后的集合对象);
+		PageInfo<TbItemParamDetail> pi = new PageInfo<>(tli);
+
+		pager.setRows(tli);
+		pager.setTotal(pi.getTotal());
+		return pager;
+	}
+
+}
+
+class TbItemParamDetail {
+	private TbItemParam tbItemParam;
+	private Long id;
+	private Long itemCatId;
+	private Date created;
+	private Date updated;
+	private String paramData;
+	private String itemCatName;
+	
+	public TbItemParam getTbItemParam() {
+		return tbItemParam;
+	}
+	public void setTbItemParam(TbItemParam tbItemParam) {
+		this.tbItemParam = tbItemParam;
+		this.id = tbItemParam.getId();
+		this.itemCatId = tbItemParam.getItemCatId();
+		this.created = tbItemParam.getCreated();
+		this.updated = tbItemParam.getUpdated();
+		this.paramData = tbItemParam.getParamData();
+	}
+	public String getItemCatName() {
+		return itemCatName;
+	}
+	public void setItemCatName(String itemCatName) {
+		this.itemCatName = itemCatName;
+	}
+	public Long getId() {
+		return id;
+	}
+	public Long getItemCatId() {
+		return itemCatId;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public Date getUpdated() {
+		return updated;
+	}
+	public String getParamData() {
+		return paramData;
+	}
+
 }
--- ebuy-manager/ebuy-web/src/main/java/com/welkin/controller/TbItemParamController.java
+++ ebuy-manager/ebuy-web/src/main/java/com/welkin/controller/TbItemParamController.java
@@ -1,7 +1,6 @@
 package com.welkin.controller;
 
-import java.util.Date;
-
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.welkin.pojo.TMessage;
+import com.welkin.pojo.Message;
+import com.welkin.pojo.Pager;
 import com.welkin.pojo.TbItemParam;
 import com.welkin.service.TbItemParamService;
 
@@ -18,43 +18,66 @@ import com.welkin.service.TbItemParamService;
 public class TbItemParamController {
 	@Autowired
 	private TbItemParamService tbItemParamService;
-	
+
+	// 删除“商品类别”对应的“规格参数”
+	@RequestMapping("/delete")
+	@ResponseBody
+	public Message delete(@RequestParam(value = "ids", required = true) ArrayList<Long> ids) {
+		Message msg = new Message();
+		int x = tbItemParamService.delete(ids);
+
+		if (x > 0)
+			msg.setStatus(200);
+		else
+			msg.setStatus(500);
+		return msg;
+	}
+
+	/**
+	 * 功能：保存商品规格信息
+	 * 
+	 * @param cid
+	 *            对应商品类型的编号
+	 * @param pstr
+	 *            商品类型对应的商品规格基本信息
+	 * @return
+	 */
+	@RequestMapping("/save/{cid}")
+	@ResponseBody
+	public Message save(@PathVariable Long cid, @RequestParam("paramData") String pstr) {
+		// 封装产品规格数据
+		TbItemParam po = new TbItemParam();
+		// 设置对应产品类型的编号
+		po.setItemCatId(cid);
+		po.setParamData(pstr);
+
+		Message msg = new Message();
+		// 调用业务中保存方法
+		int x = tbItemParamService.save(po);
+		if (x > 0)
+			msg.setStatus(200);
+		else
+			msg.setStatus(500);
+		return msg;
+	}
+
 	@RequestMapping("/query/itemcatid/{id}")
 	@ResponseBody
-	public TMessage find(@PathVariable Long id) {
+	public Message find(@PathVariable Long id) {
 		TbItemParam cat = tbItemParamService.findById(id);
-		TMessage msg = new TMessage();
+		Message msg = new Message();
 		msg.setStatus(200);
 		if (cat != null) {
 			msg.setData(cat);
 		}
 		return msg;
 	}
-	
-	/**
-	 * 功能：保存商品规格信息
-	 * @param cid 对应商品类型的编号
-	 * @param pstr 商品类型对应的商品规格基本信息
-	 * @return
-	 */
-	@RequestMapping("/save/{cid}")
+
+	@RequestMapping("/list")
 	@ResponseBody
-	public TMessage save(@PathVariable Long cid, @RequestParam("paramData") String pstr) {
-		// 封装产品规格数据
-		TbItemParam po = new TbItemParam();
-		// 设置对应产品类型的编号
-		po.setId(cid);
-		po.setParamData(pstr);
-		po.setCreated(new Date());
-		po.setUpdated(new Date());
-		// 调用业务中保存方法
-		boolean flag = tbItemParamService.save(po);
-		TMessage msg = new TMessage();
-		if(flag) 
-			msg.setStatus(200);
-		else
-			msg.setStatus(500);
-		return msg;
+	public Pager paramlist(int page, int rows) {
+		Pager p = tbItemParamService.selectPager(page, rows);
+		return p;
 	}
 
 }

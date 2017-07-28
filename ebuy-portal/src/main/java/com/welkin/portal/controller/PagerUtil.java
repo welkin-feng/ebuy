578e91376e6f5a323620e40aef74106dbf63df03
Welkin <382523558@qq.com>
2017-07-28 09:52:05 +0800

    添加一个拦截index的方法，在接到index页面请求时，去middle服务器请求数据，然后将数据和index.jsp页面返回

--- ebuy-portal/src/main/java/com/welkin/portal/controller/PagerUtil.java
+++ ebuy-portal/src/main/java/com/welkin/portal/controller/PagerUtil.java
@@ -1,8 +1,11 @@
-package com.welkin.controller;
+package com.welkin.portal.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.welkin.portal.utils.HttpClientUtils;
 
 @Controller                                                                                                                                                        
 public class PagerUtil {
@@ -12,8 +15,13 @@ public class PagerUtil {
 	}
 	
 	@RequestMapping("/index")
-	public String index() {
+	public String index(Model model) {
 		System.out.println("首页信息");
+		String url = "http://localhost:8005/content/ADList/89";
+		// 通过工具类中 doGet 方法发送请求，并接收返回的数据
+		String jsonArray = HttpClientUtils.doGet(url);
+		//将返回的json数组格式的字符串封装Model给 页面传值
+		model.addAttribute("ad1", jsonArray);
 		return "index";
 	}
 }

194f22df3eff869aa1fffdad8da960e04a3757bc
Welkin <382523558@qq.com>
2017-07-28 14:55:42 +0800

    增加 修改内容类目 和 删除内容类目 方法

--- ebuy-manager/ebuy-service/src/main/java/com/welkin/service/ContentCategoryService.java
+++ ebuy-manager/ebuy-service/src/main/java/com/welkin/service/ContentCategoryService.java
@@ -7,15 +7,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.welkin.mapper.TbContentCategoryMapper;
+import com.welkin.mapper.TbContentMapper;
 import com.welkin.pojo.TbContentCategory;
 import com.welkin.pojo.TbContentCategoryExample;
+import com.welkin.pojo.TbContentExample;
 import com.welkin.pojo.TbContentCategoryExample.Criteria;
 
 @Service
 public class ContentCategoryService {
 	@Autowired
 	private TbContentCategoryMapper tbContentCategoryMapper;
-	
+	@Autowired
+	private TbContentMapper tbContentMapper;
+
+	public int delete(Long id) {
+		// 删内容类别
+		int x1 = tbContentCategoryMapper.deleteByPrimaryKey(id);
+		// 删内容类别下面的所有内容
+		TbContentExample ex = new TbContentExample();
+		com.welkin.pojo.TbContentExample.Criteria c = ex.createCriteria();
+		c.andCategoryIdEqualTo(id);
+		int x2 = tbContentMapper.deleteByExample(ex);
+		return x1 & x2;
+	}
+
+	public int update(Long id, String name) {
+		TbContentCategory record = new TbContentCategory();
+		record.setId(id);
+		record.setName(name);
+		record.setUpdated(new Date());
+		return tbContentCategoryMapper.updateByPrimaryKeySelective(record);
+	}
+
 	public int save(TbContentCategory cc) {
 		cc.setIsParent(false);
 		cc.setStatus(1);
@@ -27,15 +50,15 @@ public class ContentCategoryService {
 		int x2 = 1;
 		// 将当前新结点对应的父节点的 IsParent 属性进行更改
 		TbContentCategory pcc = tbContentCategoryMapper.selectByPrimaryKey(cc.getParentId());
-		if(!pcc.getIsParent()){
+		if (!pcc.getIsParent()) {
 			pcc.setIsParent(true);
 			pcc.setUpdated(new Date());
 			x2 = tbContentCategoryMapper.updateByPrimaryKeySelective(pcc);
 		}
-		
+
 		return x1 & x2;
 	}
-	
+
 	public List<TbContentCategory> findByParentId(Long id) {
 		TbContentCategoryExample ex = new TbContentCategoryExample();
 		Criteria c = ex.createCriteria();

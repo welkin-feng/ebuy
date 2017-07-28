42c8a6e176fad76bcbd84f953f646a0d6432b667
Welkin <382523558@qq.com>
2017-07-28 14:55:20 +0800

    增加 修改内容 和 删除内容 方法

--- ebuy-manager/ebuy-service/src/main/java/com/welkin/service/ContentService.java
+++ ebuy-manager/ebuy-service/src/main/java/com/welkin/service/ContentService.java
@@ -9,22 +9,37 @@ import org.springframework.stereotype.Service;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.welkin.mapper.TbContentMapper;
-import com.welkin.pojo.Pager;
 import com.welkin.pojo.TbContent;
 import com.welkin.pojo.TbContentExample;
 import com.welkin.pojo.TbContentExample.Criteria;
+import com.welkin.util.Pager;
 
 @Service
 public class ContentService {
 	@Autowired
 	private TbContentMapper tbContentMapper;
-	
+
+	public int delete(List<Long> ids) {
+		TbContentExample ex = new TbContentExample();
+		Criteria c = ex.createCriteria();
+		c.andIdIn(ids);
+		return tbContentMapper.deleteByExample(ex);
+	}
+
+	public int update(TbContent con) {
+		con.setUpdated(new Date());
+		TbContentExample ex = new TbContentExample();
+		Criteria c = ex.createCriteria();
+		c.andIdEqualTo(con.getId());
+		return tbContentMapper.updateByPrimaryKeySelective(con);
+	}
+
 	public int save(TbContent con) {
 		con.setCreated(new Date());
 		con.setUpdated(new Date());
 		return tbContentMapper.insert(con);
 	}
-	
+
 	public Pager selectPager(Long cid, int page, int rows) {
 		Pager pager = new Pager();
 		PageHelper.startPage(page, rows);
@@ -32,9 +47,9 @@ public class ContentService {
 		Criteria c = ex.createCriteria();
 		c.andCategoryIdEqualTo(cid);
 		List<TbContent> li = tbContentMapper.selectByExampleWithBLOBs(ex);
-		
+
 		PageInfo<TbContent> pi = new PageInfo<>(li);
-		
+
 		pager.setRows(li);
 		pager.setTotal(pi.getTotal());
 		return pager;
--- ebuy-manager/ebuy-web/src/main/java/com/welkin/controller/ContentController.java
+++ ebuy-manager/ebuy-web/src/main/java/com/welkin/controller/ContentController.java
@@ -7,29 +7,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.welkin.pojo.Message;
-import com.welkin.pojo.Pager;
 import com.welkin.pojo.TbContent;
 import com.welkin.service.ContentService;
+import com.welkin.util.Message;
+import com.welkin.util.MessageUtil;
+import com.welkin.util.Pager;
 
 @Controller
 @RequestMapping("/content")
 public class ContentController {
 	@Autowired
 	private ContentService contentService;
-	
+
+	@RequestMapping("/delete")
+	@ResponseBody
+	public Message delete(List<Long> ids) {
+
+		// System.out.println("delete:" + "ids=" + ids);
+		int x = contentService.delete(ids);
+		return MessageUtil.compare(x);
+	}
+
+	@RequestMapping("/edit")
+	@ResponseBody
+	public Message update(TbContent con) {
+		// System.out.println("editting!");
+		// System.out.println("title:" + con.getTitle() + ",content:" +
+		// con.getContent());
+
+		int x = contentService.update(con);
+
+		return MessageUtil.compare(x);
+	}
+
 	@RequestMapping("/save")
 	@ResponseBody
 	public Message save(TbContent con) {
-		Message m = new Message();
 		int x = contentService.save(con);
-		if(x > 0) 
-			m.setStatus(200);
-		else
-			m.setStatus(500);
-		return m;
+		return MessageUtil.compare(x);
 	}
-	
+
 	@RequestMapping("/query/list")
 	@ResponseBody
 	public Pager findByCid(Long categoryId, int page, int rows) {

755cc9d477012c2b39df5a29b3cf8b5c0568daf7
qin <qin@qin-pc>
2017-07-29 23:34:11 +0800

    Update format

--- ebuy-manager/ebuy-service/src/main/java/com/welkin/service/ImageUploaderService.java
+++ ebuy-manager/ebuy-service/src/main/java/com/welkin/service/ImageUploaderService.java
@@ -16,9 +16,9 @@ import com.jcraft.jsch.ChannelSftp;
 @Service
 public class ImageUploaderService {
 	private String username = "ftpuser";
-	private String host = "10.211.55.7";
+	private String host = "192.168.254.136";
 	private int port = 22;
-	private String userpwd = "ftpuser";
+	private String userpwd = "123456";
 	private String baseDir="/home/ftpuser/www";
 
 	/**

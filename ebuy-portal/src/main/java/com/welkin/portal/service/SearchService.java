bb77556e95cf9326af6fe68de35a0a049b7ce6f8
Welkin <382523558@qq.com>
2017-07-30 14:28:03 +0800

    Update format

--- ebuy-portal/src/main/java/com/welkin/portal/service/SearchService.java
+++ ebuy-portal/src/main/java/com/welkin/portal/service/SearchService.java
@@ -3,6 +3,7 @@ package com.welkin.portal.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.welkin.commons.Message;

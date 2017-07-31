3fda6f551abdc32cf677fa36e0512c5738af9fad
Welkin <382523558@qq.com>
2017-07-31 15:32:47 +0800

    添加登出功能

--- ebuy-portal/src/main/java/com/welkin/portal/controller/UserController.java
+++ ebuy-portal/src/main/java/com/welkin/portal/controller/UserController.java
@@ -1,42 +1,19 @@
 package com.welkin.portal.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.welkin.portal.utils.HttpClientUtils;
+import com.welkin.commons.CookieUtils;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
 
-	@RequestMapping("/showLogin")
-	public String showLogin() {
-		return "redirect:/login.html";
+	@RequestMapping("/logout")
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		CookieUtils.deleteCookie(request, response, "TT_TOKEN");
+		return "index";
 	}
-	
-	@RequestMapping("/showRegister")
-	public String showRegister() {
-		return "redirect:/register.html";
-	}
-	
-	@RequestMapping("/login")
-	@ResponseBody
-	public String login(String username, String password) {
-		
-		String url = "http://localhost:8084/user/login";
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("username", username);
-		params.put("password", password);
-		String jsonStr = HttpClientUtils.doPost(url, params);
-		
-		
-		return jsonStr;
-	}
+
 }

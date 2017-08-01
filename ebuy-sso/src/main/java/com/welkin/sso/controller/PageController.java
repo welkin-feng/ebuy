6fc567d7fa40894bffdadd22667c925fd6bd427a
Welkin <382523558@qq.com>
2017-08-01 10:11:14 +0800

    Update format

--- ebuy-sso/src/main/java/com/welkin/sso/controller/PageController.java
+++ ebuy-sso/src/main/java/com/welkin/sso/controller/PageController.java
@@ -1,13 +1,10 @@
 package com.welkin.sso.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.welkin.commons.CookieUtils;
 
 
 @Controller

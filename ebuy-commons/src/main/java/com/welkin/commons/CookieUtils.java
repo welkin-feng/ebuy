7c0544ddafb04d3e491677629503bc9b0c5c2baf
Welkin <382523558@qq.com>
2017-08-04 00:00:07 +0800

    修改域名解析判断条件

--- ebuy-commons/src/main/java/com/welkin/commons/CookieUtils.java
+++ ebuy-commons/src/main/java/com/welkin/commons/CookieUtils.java
@@ -200,19 +200,35 @@ public final class CookieUtils {
         if (serverName == null || serverName.equals("")) {
             domainName = "";
         } else {
+        	// System.out.println("serverName1 : " + serverName);
             serverName = serverName.toLowerCase();
+            // System.out.println("serverName2 : " + serverName);
             serverName = serverName.substring(7);
+            // System.out.println("serverName3 : " + serverName);
             final int end = serverName.indexOf("/");
             serverName = serverName.substring(0, end);
+            // System.out.println("serverName4 : " + serverName);
             final String[] domains = serverName.split("\\.");
+            
             int len = domains.length;
             if (len > 3) {
-                // www.xxx.com.cn
-                domainName = "." + domains[len - 3] + "." + domains[len - 2] + "." + domains[len - 1];
+            	domainName = "";
+            	System.out.println("len > 3");
+            	String[] lastName = domains[len - 1].split("\\:");
+            	if (lastName.length > 1){
+            		for(int i = 0; i < len - 1; i ++)
+            			domainName += domains[i] + ".";
+            		domainName += lastName[0];
+            	} else {
+            		// www.xxx.com.cn
+            		domainName = "." + domains[len - 3] + "." + domains[len - 2] + "." + domains[len - 1];
+            	}
             } else if (len <= 3 && len > 1) {
+            	System.out.println("len < 3");
                 // xxx.com or xxx.cn
                 domainName = "." + domains[len - 2] + "." + domains[len - 1];
             } else {
+            	System.out.println("len = 0");
                 domainName = serverName;
             }
         }
